979252b0342aa58f9a7f0e4b955296601a235396
DanyalBani <mr.d.bani.d@gmail.com>
2022-02-25 09:58:18 +0330

    nav toggle changed from hidden to active (easy animation)

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const burger = document.querySelector(".burger");
 const nav = document.querySelector(".nav");
 burger.addEventListener("click", () => {
     burger.classList.toggle("active");
+    nav.classList.toggle("active");
 });
